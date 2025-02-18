1949e638a5e7ae7e41f321c66dd19a1b77a0f214
sykan <sykan@DELL-Pavilion>
2025-02-18 21:05:40 +0900

    sayAgeを追加しました

--- IA/src/internetAcademy/Person.java
+++ IA/src/internetAcademy/Person.java
@@ -1,20 +1,34 @@
 package internetAcademy;
 
 public class Person {
-	
+
 	String name;
+	String sex;
 	int age;
-	
-	public Person(String name, int age) {
-		
+
+	public Person(String name, String sex, int age) {
+
 		this.name = name;
+		this.sex = sex;
 		this.age = age;
-		
+
 		System.out.println("オギャー" + "、" + this.age + "歳の誕生です。");
 	}
-	
+
 	public void sayHello() {
-		System.out.println("こんにちは" + this.name + "です。" + this.age + "歳です。");
+		System.out.println("こんにちは" + this.name + "です。" + this.age + "歳です。" + this.sex + "です。");
+	}
+
+	public void sayAge() {
+
+		if (age < 20) {
+			System.out.println(this.name + "はこれから成長期です。");
+		} else if (age >= 20 && age <= 50) {
+			System.out.println(this.name + "は働き盛りです。");
+		} else {
+			System.out.println(this.name + "はもう引退です");
+		}
+
 	}
 
 }
--- IA/src/internetAcademy/Sample.java
+++ IA/src/internetAcademy/Sample.java
@@ -2,20 +2,17 @@ package internetAcademy;
 
 public class Sample {
 
-	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
+    public static void main(String[] args) {
 
-		
-		Person p1 = new Person("タロウ",8);
-		Person p2 = new Person("ユウタ",12);
-		Person p3 = new Person("ダイチ",18);
-		
-	
-		
-		p1.sayHello();
-		p2.sayHello();
-		p3.sayHello();
-		
-	}
+        Person[] people = {
+                new Person("タロウ", "男性", 8),
+                new Person("ユウコ", "女性", 30),
+                new Person("ダイチ", "男性", 55)
+        };
 
+        for (Person person : people) {
+            person.sayHello();
+            person.sayAge();
+        }
+    }
 }
